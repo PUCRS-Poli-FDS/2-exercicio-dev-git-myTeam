ca5623512648256a0127926d6c8aeaed3cb06c83
Mario Specht <ms.specht@gmail.com>
2019-08-27 20:54:20 -0300

    Commit Inicial

---
+++ marsrover/src/main/java/br/pucrs/marsrover/MarsroverApplication.java
@@ -0,0 +1,13 @@
+package br.pucrs.marsrover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsroverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarsroverApplication.class, args);
+    }
+
+}
